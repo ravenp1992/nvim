60e667ce562b353daa57c1e0cc0f143df2306c5d
Raven Paragas <ravenparagas@Ravens-MacBook-Pro.local>
2024-06-20 22:19:05 +0800

    update nvim config

--- lua/raven/plugins/bufferline.lua
+++ lua/raven/plugins/bufferline.lua
@@ -2,10 +2,10 @@ return {
 	"akinsho/bufferline.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	version = "*",
-	keys = {
-		{ "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
-		{ "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
-	},
+	-- keys = {
+	-- 	{ "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
+	-- 	{ "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
+	-- },
 	config = function()
 		local bufferline = require("bufferline").setup()
 	end,
